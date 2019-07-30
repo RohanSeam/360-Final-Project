2aee4b577617de9142585815522649b83fa11d25
RohanSeam <53382888+RohanSeam@users.noreply.github.com>
2019-07-29 23:37:18 -0700

    Update WeatherStationInt.java

--- src/components/weatherstation/WeatherStationInt.java
+++ src/components/weatherstation/WeatherStationInt.java
@@ -1,5 +1,8 @@
-package components.weatherstation;
+//package components.weatherstation;
 
-public interface WeatherStationInt
-{
+public interface WeatherStationInt {
+   public void storeData(String dataItem, String type);
+   public void processData(String dataItem, String type);
+   public void sendData();
+   public void transferData(String externalDevice);
 }
